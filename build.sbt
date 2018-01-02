3de6448696e5b78a1e8eb0f9369081e7efd52dfb
khavelun <klaus.havelund@jpl.nasa.gov>
2018-01-02 10:53:42 -0800

    Updated with a new mesa example.

--- build.sbt
+++ build.sbt
@@ -2,7 +2,11 @@ name := "TraceContract"
 
 version := "1.0"
 
+// Scala:
+
 scalaVersion := "2.12.1"
+
+// Akka:
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.4.16"
---
+++ src/test/scala/examples/mesa/Example.scala
@@ -0,0 +1,83 @@
+package examples.mesa
+
+import tracecontract._
+
+trait Event
+
+case class Waypoint(num: Long) extends Event
+
+case object Other extends Event
+
+object Event {
+  val W1 = 1
+  val W2 = 2
+  val W3 = 3
+}
+
+import Event._
+
+class WaypointMonitor extends Monitor[Event] {
+  property('waypoints_ordering_statelogic) {
+    state {
+      case Waypoint(W2) | Waypoint(W3) => error
+      case Waypoint(W1) =>
+        state {
+          case Waypoint(W1) | Waypoint(W3) => error
+          case Waypoint(W2) =>
+            state {
+              case Waypoint(W1) | Waypoint(W2) => error
+              case Waypoint(W3) => ok("Property waypoints_ordering is satisfied")
+            }
+        }
+    }
+  }
+}
+
+class StrongWaypointMonitor1 extends Monitor[Event] {
+  property('waypoints_ordering_statelogic) {
+    state {
+      case Waypoint(W2) | Waypoint(W3) => error
+      case Waypoint(W1) =>
+        strong {
+          case Waypoint(W2) =>
+            strong {
+              case Waypoint(W3) => ok("Property waypoints_ordering is satisfied")
+            }
+        }
+    }
+  }
+}
+
+class StrongWaypointMonitor2 extends Monitor[Event] {
+  select { case Waypoint(_) => true }
+
+  property('waypoints_ordering_statelogic) {
+    strong {
+      case Waypoint(W1) =>
+        strong {
+          case Waypoint(W2) =>
+            strong {
+              case Waypoint(W3) => ok("Property waypoints_ordering is satisfied")
+            }
+        }
+    }
+  }
+}
+
+object TraceAnalysis {
+  def main(args: Array[String]) {
+    def trace: List[Event] =
+      List(
+        Other,
+        Waypoint(W1),
+        Other,
+        Waypoint(W2),
+        Other,
+        Waypoint(W3),
+        Other
+      )
+
+    val monitor = new StrongWaypointMonitor2
+    monitor.verify(trace)
+  }
+}
